5f9f64391f3453589f3ee687008a603d192d7c65
Shahil AppDev <shahil.boodhoye@gmail.com>
2026-01-03 00:43:10 +0100

    Optimisations Safari/iPhone: webkit prefixes, hardware acceleration, video inline

--- components/background/VideoBackground.tsx
+++ components/background/VideoBackground.tsx
@@ -8,7 +8,10 @@ export default function VideoBackground() {
         loop
         muted
         playsInline
+        webkit-playsinline="true"
+        preload="auto"
         className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 object-cover opacity-40"
+        style={{ WebkitTransform: 'translateZ(0)' }}
       >
         <source src="/background-video.mp4" type="video/mp4" />
       </video>
