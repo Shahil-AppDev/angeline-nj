0728405cecdc2ea33d73f51d8b8d194dcb0d9156
Shahil AppDev <shahil.boodhoye@gmail.com>
2026-01-02 10:00:15 +0100

    Refactor: Remplacement de angeline-nj.fr par angeline-nj.xyz

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://angeline-nj.fr/sitemap.xml',
+    sitemap: 'https://angeline-nj.xyz/sitemap.xml',
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://angeline-nj.fr';
+  const baseUrl = 'https://angeline-nj.xyz';
 
   return [
     {
