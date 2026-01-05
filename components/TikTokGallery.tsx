c4c6b4926a6c5b8e085f302534b925c6a726c5d0
Shahil AppDev <shahil.boodhoye@gmail.com>
2026-01-05 17:18:56 +0100

    feat: ajouter 13eme video TikTok (Tirage NJ relation passionnelle)

--- components/TikTokGallery.tsx
+++ components/TikTokGallery.tsx
@@ -18,9 +18,9 @@ const tiktokVideos = [
   'https://www.tiktok.com/@angelinenj/video/7589334050016349443', // Gossip Love réconciliation
   'https://www.tiktok.com/@angelinenj/video/7588850210856504598', // Tirage sentimental amour
   'https://www.tiktok.com/@angelinenj/video/7588538474744007958', // 9 oracles promo Noël
-  'https://www.tiktok.com/@angelinenj/video/7588538474744007958',
-  'https://www.tiktok.com/@angelinenj/video/7588538474744007958',
-  'https://www.tiktok.com/@angelinenj/video/7588538474744007958',
+  'https://www.tiktok.com/@angelinenj/video/7588202649875614999', // Tirage NJ relation passionnelle
+  'https://www.tiktok.com/@angelinenj/video/7588202649875614999',
+  'https://www.tiktok.com/@angelinenj/video/7588202649875614999',
 ];
 
 export default function TikTokGallery() {
